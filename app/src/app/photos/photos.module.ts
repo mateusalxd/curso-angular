7f918e46f6e16602466deb950af8f4333324a776
mateusalxd <mateusalxd@gmail.com>
2020-02-08 15:23:31 -0300

    Criação de um pipe personalizado

--- app/src/app/photos/photos.module.ts
+++ app/src/app/photos/photos.module.ts
@@ -6,9 +6,10 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
 
 @NgModule({
-    declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent],
+    declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescriptionPipe],
     imports: [
         HttpClientModule,
         CommonModule
